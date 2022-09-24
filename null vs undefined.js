e65318100e93a5b7c256125aca29de8e6aaef22e
rejhaque <rij.rezwan@gmail.com>
2022-09-24 19:15:12 +0600

    null vs undefined

---
+++ null vs undefined.js
@@ -0,0 +1,20 @@
+let pakhi;
+
+function add(num1, num2) {
+     console.log(num1 + num2);
+     return
+};
+
+function add(num1, num2){
+     console.log(num1, num2);
+}
+
+const rejwan = {name: "Austin", phone: 01325013000};
+console.log(rejwan.gf);
+
+let fun = undefined;
+console.log(fun);
+
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
