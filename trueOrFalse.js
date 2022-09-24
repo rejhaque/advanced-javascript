4b2538ebdc3cfb0520c828f18356cae1b833e5a0
rejhaque <rij.rezwan@gmail.com>
2022-09-24 18:56:59 +0600

    true false conditions.

---
+++ trueOrFalse.js
@@ -0,0 +1,18 @@
+// Falsy:
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+//Truethy:
+//'0' , ' ' , [] , 'false'    
+const name = Rejwanu ;
+
+if(age > 0){
+     console.log("condition is true");
+}
+else{
+     console.log("condition is false");
+}
+
